bada418a431c61f2752c4a15edd749a69397dfb2
Diego Trejos <d.trejosh@gmail.com>
2026-05-15 12:43:19 -0600

    feat audio service changed

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -3,6 +3,14 @@ import 'package:just_audio/just_audio.dart';
 class AudioService {
   final player = AudioPlayer();
 
+  Future<void> startAlarm(
+      {required String assetPath, required double volume}) async {
+    await player.setLoopMode(LoopMode.one);
+    await player.setVolume(volume);
+    await player.setAsset(assetPath);
+    await player.play();
+  }
+
   Future<void> play(String sound) async {
     await player.setAsset(sound);
     await player.play();
@@ -10,5 +18,9 @@ class AudioService {
 
   Future<void> stop() async {
     await player.stop();
+  }
+
+  void dispose() {
+    player.dispose();
   }
 }
