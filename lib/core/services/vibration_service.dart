cb9ca61137881889a6e5ffdd490a23127f267eb1
Diego Trejos <d.trejosh@gmail.com>
2026-05-16 10:51:25 -0600

    feat nuevo vibration service y provider

---
+++ lib/core/services/vibration_service.dart
@@ -0,0 +1,15 @@
+class VibrationService {
+  Future<void> start() async {
+    final hasVibrator = await Vibration.hasVibrator();
+    if (hasVibrator != true) return;
+
+    Vibration.vibrate(
+      pattern: [0, 1000, 500],
+      repeat: 0,
+    );
+  }
+
+  Future<void> stop() async {
+    Vibration.cancel();
+  }
+}
